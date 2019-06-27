d2df29199058bfad361d18dbf56f8fda32618618
osandul <sashka95.13@gmail.com>
2019-06-27 15:28:02 +0300

    in process of adding of cucumber context

--- src/test/java/steps/StepDefs.java
+++ src/test/java/steps/StepDefs.java
@@ -1,16 +1,19 @@
 package steps;
 
 import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 import static io.restassured.RestAssured.*;
+
 import io.restassured.path.json.JsonPath;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import org.testng.Assert;
 import properties.PropertyLoader;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,17 +26,15 @@ public class StepDefs {
 
     @When("I create a new board with name {name}")
     public void i_create_a_new_board_with_name(Name name) {
-       // this.boardName = boardName;
         Map<String, String> params = new HashMap<>();
-        params.put("name", String.valueOf(name));
+        params.put("name", name.toString());
         params.put("key", PropertyLoader.getProperty("key"));
         params.put("token", PropertyLoader.getProperty("token"));
 
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
 
-
-        response = given().headers(headers).queryParams(params).when().post(PropertyLoader.getProperty("ROOT_API_URL")+ "1/boards/");
+        response = given().headers(headers).queryParams(params).when().post(PropertyLoader.getProperty("ROOT_API_URL") + "1/boards/");
     }
 
     @Then("I check that server handles it and returns a success status")
@@ -41,10 +42,17 @@ public class StepDefs {
         json = response.then().assertThat().statusCode(200);
     }
 
-    @And("I check that the {name} is set correctly")
+    @And("I check that the name {name} is set correctly")
     public void checksThatTheNameIsSetCorrectly(Name name) {
         JsonPath jsonPathEvaluator = response.jsonPath();
-        name = jsonPathEvaluator.get("name");
-        Assert.assertTrue(name.equals(name));
+       boardName = jsonPathEvaluator.get("name");
+        Assert.assertEquals(boardName, name.toString());
     }
+
+    @And("I get id of board {name}")
+    public void iGetIdOfBoardBOARD()  {
+        JsonPath jsonPathEvaluator = response.jsonPath();
+   //     boardId = jsonPathEvaluator.get("id");
+
+        }
 }
--- src/test/resources/cucumber/creatingBoard.feature
+++ src/test/resources/cucumber/creatingBoard.feature
@@ -2,12 +2,14 @@ Feature: Board management
 
 Scenario: Create a new board
 
-  When I create a new board with name {name}
+  When I create a new board with name BOARD
   Then I check that server handles it and returns a success status
-  And I check that the {name} is set correctly
+  And I check that the name BOARD is set correctly
+  And I get id of board BOARD
 
-#  Scenario: Create a list
-#
-#    When I create a new list with name "To do:"
-#    Then I check that server handles it and returns a success status
-#    And I check that the {name} is set correctly
+  Scenario: Create a list
+
+
+    When I create a new list with name "To do:"
+    Then I check that server handles it and returns a success status
+    And I check that the {name} is set correctly
