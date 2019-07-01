1f415f8c877c7d9409a8742bcce45e9d52564b32
osandul <sashka95.13@gmail.com>
2019-07-01 11:55:42 +0300

    "create List" scenario added

--- src/test/java/steps/StepDefs.java
+++ src/test/java/steps/StepDefs.java
@@ -24,19 +24,19 @@ public class StepDefs {
     private Response response;
     private ValidatableResponse json;
     private RequestSpecification request;
-    private String boardName;
+
 
     @When("I create a new board with name {name}")
     public void i_create_a_new_board_with_name(Name name) {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", name.toString());
-        params.put("key", PropertyLoader.getProperty("key"));
-        params.put("token", PropertyLoader.getProperty("token"));
+        Map<String, String> boardParams = new HashMap<>();
+        boardParams.put("name", name.toString());
+        boardParams.put("key", PropertyLoader.getProperty("key"));
+        boardParams.put("token", PropertyLoader.getProperty("token"));
 
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
 
-        response = given().headers(headers).queryParams(params).when().post(PropertyLoader.getProperty("ROOT_API_URL") + "1/boards/");
+        response = given().headers(headers).queryParams(boardParams).when().post(PropertyLoader.getProperty("ROOT_API_URL") + "1/boards/");
     }
 
     @Then("I check that server handles it and returns a success status")
@@ -47,14 +47,27 @@ public class StepDefs {
     @And("I check that the name {name} is set correctly")
     public void checksThatTheNameIsSetCorrectly(Name name) {
         JsonPath jsonPathEvaluator = response.jsonPath();
-       boardName = jsonPathEvaluator.get("name");
-        Assert.assertEquals(boardName, name.toString());
+        Assert.assertEquals(jsonPathEvaluator.get("name"), name.toString());
     }
 
     @And("I get id of board and save to context")
-    public void iGetIdOfBoardBOARD()  {
+    public void iGetIdOfBoardAndSaveToContext()  {
         JsonPath jsonPathEvaluator = response.jsonPath();
         ScenarioContext.setContext(BOARD_ID,jsonPathEvaluator.get("id"));
         }
 
+    @When("I create a new list with name {name}")
+    public void iCreateANewListWithNameTO_DO(Name name)  {
+            Map<String, String> listParams = new HashMap<>();
+            listParams.put("name", name.toString());
+            listParams.put("idBoard", ScenarioContext.getContext(BOARD_ID).toString());
+            listParams.put("key", PropertyLoader.getProperty("key"));
+            listParams.put("token", PropertyLoader.getProperty("token"));
+
+            Map<String, String> headers = new HashMap<>();
+            headers.put("Content-Type", "application/json");
+
+            response = given().headers(headers).queryParams(listParams).when().post(PropertyLoader.getProperty("ROOT_API_URL") + "1/lists");
+
+    }
 }
--- src/test/resources/cucumber/creatingBoard.feature
+++ src/test/resources/cucumber/creatingBoard.feature
@@ -11,6 +11,6 @@ Scenario: Create a new board
   Scenario: Create a list
 
 
-    When I create a new list with name "To do:"
+    When I create a new list with name TO_DO
     Then I check that server handles it and returns a success status
-    And I check that the {name} is set correctly
+    And I check that the name TO_DO is set correctly
