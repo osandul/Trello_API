dfc696b1e8891ff00f44f165d86f4adaf4c4bbad
osandul <sashka95.13@gmail.com>
2019-07-01 17:39:50 +0300

    in progress of adding allure reporting

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -2,7 +2,9 @@ package runners;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.Test;
 
+@Test
 @CucumberOptions(
         features = "src/test/resources",
         glue = {"steps"},
